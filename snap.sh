058d5bd6b4d3fc9e3318018eb5a33c611f6690af
root <root@ip-172-31-91-28.ec2.internal>
2020-03-24 17:47:28 +0000

    test

--- snap.sh
+++ snap.sh
@@ -1,20 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
-#cd /home/ubuntu/snap2html/
+#扫描
 
-#python3 /home/ubuntu/snap2html/diogeneslist.py /home/ubuntu/GoogleDrive index
+cd /home/ubuntu/snap2html/
 
-#sed -i "s#\[LINK ROOT\]#https://www.flygd.ga#g" /home/ubuntu/snap2html/index.html
+python3 /home/ubuntu/snap2html/diogeneslist.py /home/ubuntu/GoogleDrive index
 
-#sed -i "s#\[LINK PROTOCOL\]##g" /home/ubuntu/snap2html/index.html
-
-#sed -i "s#\[SOURCE ROOT\]##g" /home/ubuntu/snap2html/index.html
-
-#sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\GoogleDrive##g" /home/ubuntu/snap2html/index.html
-
-#sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用_侵权联系https://t.me/Butterfly_AF(telegram)!#g" /home/ubuntu/snap2html/index.html
-
-#sed -i "s#e>index#e>TG群组：https://t.me/Butterfly_EFYS" /home/ubuntu/snap2html/index.html
+#扫描完推送
 
 cp /home/ubuntu/snap2html/index.html /home/ubuntu/snapGit/
 
@@ -25,4 +17,3 @@ git add .
 git commit -m 'snap2htmlDailyUpdate'
 
 git push https://github.com/plmxs2017/snap2html.git
-
